deef2556b1dd8294e76a99106ad16318487c2e80
Leon <leon.muriuki@student.moringaschool.com>
2023-11-26 12:21:55 +0300

    Completed Lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,19 @@
 // Code Keypad Component Here
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+import React from 'react';
+
+function Keypad() {
+  // Event handler for the change event
+  const handleChange = () => {
+    console.log('Entering password...');
+  };
+
+  return (
+    <div>
+      {/* Input with type="password" and event handler */}
+      <input type="password" onChange={handleChange} />
+    </div>
+  );
 }
 
 export default Keypad;
